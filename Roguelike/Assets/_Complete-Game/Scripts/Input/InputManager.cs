76239d8436df14c0ddebfd194519974f77fc66f9
GabrielFSilva <gabrielsilva.prog@gmail.com>
2019-03-24 19:47:05 -0300

    Roguelike Input

---
+++ Roguelike/Assets/_Complete-Game/Scripts/Input/InputManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Completed;
+
+public class InputManager : MonoBehaviour
+{
+    public Player player;
+    
+    public void MovePlayer(float hAxis, float vAxis)
+    {
+        player.Move(hAxis, vAxis);
+    }
+}
